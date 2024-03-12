bbd1d56d4532206542751cb58a895847db6b1e14
Chunkybanana <chunkybananagithub@gmail.com>
2024-03-13 10:27:41 +1300

    unfixed 'fixed' 0s at end of input

--- positionally.js
+++ positionally.js
@@ -4,7 +4,8 @@ let positionally = (code, inputs = [], flags = '', output = x => process.stdout.
     let maxlength = Math.max(...code.map(x => x.length));
     code = code.map(x => x.padEnd(maxlength, ' ').split``);
 
-    inputs = inputs.flatMap(x => typeof x == 'string' ? x.split``.map(x => x.charCodeAt()).concat(0) : x) || [0];
+    inputs = inputs.flatMap(x => typeof x == 'string' ? x.split``.map(x => x.charCodeAt()).concat(0) : x)
+    if (inputs.length == 0) inputs = [0];
 
     class Ip {
         constructor (x, y) {
